53f419d8a746dd616eb7d2c92d19eaad6a3a1441
Dalleau Théo <theoferah@gmail.com>
2020-05-31 03:10:59 +0200

    feature(users): add authentication and profile informations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,29 @@
 class User < ApplicationRecord
-  validates :username, uniqueness: true, presence: true
+  has_secure_password
+
   has_many :quests, foreign_key: :quest_author_id
   has_many :quests, foreign_key: :quest_hunter_id
+
+  before_validation :generate_uuid, on: :create
+
+  validates :username, uniqueness: true, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, uniqueness: true, presence: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+  validates :phone_number, presence: true, phone: { possible: true, countries: [:fr, :be, :ch, :ca]}
+
+  validates_length_of :password, within: 8..20
+
+  def generate_uuid
+    self.uuid = SecureRandom.uuid
+  end
+
+  def to_token_payload
+    {
+      sub: uuid,
+      email: email
+    }
+  end
 end
--- db/migrate/20200527151438_add_uuid_to_quests.rb
+++
@@ -1,5 +0,0 @@
-class AddUuidToQuests < ActiveRecord::Migration[5.1]
-  def change
-    add_column :quests, :uuid, :string, default: ""
-  end
-end
