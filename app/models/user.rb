11a4bffa0d8abcc0a1a8a11c0981eec7f10991a3
Hassan Ariz <>
2020-06-03 01:06:47 +0200

    add password and username restriction

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,20 +1,23 @@
 class User < ApplicationRecord
   has_secure_password
 
+  PASSWORD_FORMAT = /\A.*(?=.{10,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W]).*\z/
   has_many :quests, foreign_key: :quest_author_id
   has_many :quests, foreign_key: :quest_hunter_id
 
   before_validation :generate_uuid, on: :create
 
-  validates :username, uniqueness: true, presence: true
+  validates :username, uniqueness: true, presence: true, format: { with: /\A[a-zA-Z0-9_]+\z/, message: I18n.t("api.invalid_username")}
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, uniqueness: true, presence: true, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/ }
-  validates :password, presence: true
+  validates :password, presence: true, format: { with: PASSWORD_FORMAT, message: I18n.t("api.invalid_password") }
   validates :password_confirmation, presence: true
-  validates :phone_number, presence: true, phone: { possible: true, countries: [:fr, :be, :ch, :ca]}
+  validates :phone_number, presence: true, phone: { possible: true, countries: [:fr, :be, :ca]}
 
-  validates_length_of :password, within: 8..20
+  validates_length_of :password, within: 8..64
+
+
 
   def generate_uuid
     self.uuid = SecureRandom.uuid
@@ -30,5 +33,9 @@ class User < ApplicationRecord
 
   def stored_quests
     self.created_quests.where(stored: true)
+  end
+
+  def password_validation
+    self.password.include?
   end
 end
