83b18c457d4177ece35d80b7b452cea175c1b8ca
Dalleau Théo <theoferah@gmail.com>
2020-06-04 12:07:46 +0200

    validation(quest): add some validation to model side

--- app/models/quest.rb
+++ app/models/quest.rb
@@ -5,6 +5,9 @@ class Quest < ApplicationRecord
   belongs_to :quest_author, class_name: :User, foreign_key: :quest_author_id
   belongs_to :quest_hunter, class_name: :User, foreign_key: :quest_hunter_id, optional: true
 
+  validates :title, presence: true
+  validates :quest_author, presence: true
+
   before_validation :generate_uuid, on: :create
 
   def generate_uuid
